529c34100573121020dc4dd1b6580449828de083
matvey <matveyvs1987@gmail.com>
2023-07-20 16:45:59 +0200

    feat: Create Redis Dependency and RedisConfig class

---
+++ src/main/java/com/matveyvs/chitchatbot/config/RedisConfig.java
@@ -0,0 +1,34 @@
+package com.matveyvs.chitchatbot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory(){
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName("localhost");
+        redisStandaloneConfiguration.setPort(5379);
+
+        return new JedisConnectionFactory(redisStandaloneConfiguration);
+    }
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(){
+        RedisTemplate<String,Object> template = new RedisTemplate<>();
+
+        template.setConnectionFactory(jedisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashKeySerializer(new JdkSerializationRedisSerializer());
+        template.setValueSerializer(new JdkSerializationRedisSerializer());
+        template.setEnableTransactionSupport(true);
+        template.afterPropertiesSet();
+        return template;
+    }
+}
